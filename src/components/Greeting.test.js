839978ae93120c9f81074636735d2bf7a782ef14
kscheltema <k87scheltema@outlook.com>
2021-07-15 09:10:26 +0200

    discribe test group

--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -1,14 +1,19 @@
 import { render, screen } from "@testing-library/react";
 import Greeting from "./Greeting";
 
-test("renders -Hello World- as a text", () => {
-  //arrange -set up the test data
-  render(<Greeting />);
+describe("Greeting component", () => {
+  test("renders -Hello World- as a text", () => {
+    //arrange -set up the test data
+    render(<Greeting />);
 
-  //act - run the logic - like a button test
-  //none
+    //act - run the logic - like a button test
+    //none
 
-  //assert - compares execution results with expected results
-  const helloWorld = screen.getByText("Hello World", { exact: false });
-  expect(helloWorld).toBeInTheDocument();
+    //assert - compares execution results with expected results
+    const helloWorld = screen.getByText("Hello World", { exact: false }); //exact false = check within /default harsher check
+    expect(helloWorld).toBeInTheDocument();
+  });
 });
+
+//testing suites
+//group together different components/functions or features
